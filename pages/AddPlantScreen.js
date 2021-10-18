6880a3e6ee70d0e59d302b6206b692e6ffb9e9ab
Ginzd <ginzmod@gmail.com>
2021-10-18 18:52:31 +0700

    Edit Test

--- pages/AddPlantScreen.js
+++ pages/AddPlantScreen.js
@@ -1,105 +1,143 @@
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  Button
-} from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
 import DatePicker from "react-native-datepicker";
 const AddPlantScreen = () => {
-  const [text, onChangeText] = React.useState("Dia diem");
+  const [text, onChangeText] = React.useState(null);
   const [number, onChangeNumber] = React.useState(null);
   const [date, setDate] = useState("09-10-2020");
 
   return (
     <View style={styles.dropdownn}>
       <Text style={styles.labelText}>Vị trí canh tác</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="Dien tich"
-        keyboardType="numeric"
-      />
+      <View style={styles.rowContainer}>
+        <Text style={styles.text}>Địa điểm</Text>
+        <TextInput
+          style={styles.textInput}
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+      </View>
+      <View style={styles.rowContainer}>
+        <Text style={styles.text}>Diện tích</Text>
+        <TextInput
+          style={styles.textInput}
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+      </View>
+
       <Text style={styles.labelText}>Mùa vụ</Text>
-      <Text style={styles.simpleText}>Giong cay</Text>
 
-      <RNPickerSelect
-        placeholder={{ label: "Select plant", value: null }}
-        onValueChange={(value) => console.log(value)}
-        items={[
-          { label: "CaChua", value: "cachua" },
-          { label: "Baseball", value: "baseball" },
-          { label: "Hockey", value: "hockey" },
-        ]}
-      />
-      <Text style={styles.simpleText}>Ngay bat dau</Text>
-      <DatePicker
-        style={styles.datePickerStyle}
-        date={date} // Initial date from state
-        mode="date" // The enum of date, datetime and time
-        placeholder="select date"
-        format="DD-MM-YYYY"
-        minDate="01-01-2016"
-        maxDate="01-01-2019"
-        confirmBtnText="Confirm"
-        cancelBtnText="Cancel"
-        customStyles={{
-          dateIcon: {
-            //display: 'none',
-            position: "absolute",
-            left: 0,
-            top: 4,
-            marginLeft: 0,
-          },
-          dateInput: {
-            marginLeft: 36,
-          },
-        }}
-        onDateChange={(date) => {
-          setDate(date);
-        }}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="So luong trong"
-        keyboardType="numeric"
-      />
+      <View style={styles.rowContainer}>
+        <Text style={styles.simpleText}>Giống cây</Text>
+        <View style={styles.rowContainerPicker}>
+          <RNPickerSelect
+            placeholder={{ label: "Lựa chọn cây", value: null }}
+            onValueChange={(value) => console.log(value)}
+            items={[
+              { label: "Cà chua", value: "cachua" },
+              { label: "Cà pháo", value: "baseball" },
+              { label: "Cà na", value: "hockey" },
+            ]}
+          />
+        </View>
+      </View>
+      <View style={styles.rowContainer}>
+        <Text style={styles.simpleText}>Ngày bắt đầu</Text>
+        <DatePicker
+          style={styles.datePickerStyle}
+          date={date} // Initial date from state
+          mode="date" // The enum of date, datetime and time
+          placeholder="select date"
+          format="DD-MM-YYYY"
+          minDate="01-01-2016"
+          maxDate="01-01-2019"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          customStyles={{
+            dateIcon: {
+              //display: 'none',
+              position: "absolute",
+              left: 0,
+              top: 4,
+              marginLeft: 0,
+            },
+            dateInput: {
+              marginLeft: 36,
+            },
+          }}
+          onDateChange={(date) => {
+            setDate(date);
+          }}
+        />
+      </View>
 
-      <Text style={styles.simpleText}>Don vi tinh</Text>
+      <View style={styles.rowContainer}>
+        <Text style={styles.text}>Số lượng trồng</Text>
+        <TextInput
+          style={styles.textInput}
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+        />
+      </View>
 
-      <RNPickerSelect
-        placeholder={{ label: "Select ....", value: null }}
-        onValueChange={(value) => console.log(value)}
-        items={[
-          { label: "klg", value: "klg" },
-          { label: "hat", value: "hat" },
-        ]}
-      />
-          <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="Ghi chu"
-        keyboardType="numeric"
-      />
-      <Button title="SAVE"/>
+      <View style={styles.rowContainer}>
+        <Text style={styles.simpleText}>Đơn vị tính</Text>
+        <View style={styles.rowContainerPicker}>
+          <RNPickerSelect
+            placeholder={{ label: "Select ....", value: null }}
+            onValueChange={(value) => console.log(value)}
+            items={[
+              { label: "klg", value: "klg" },
+              { label: "hat", value: "hat" },
+            ]}
+          />
+        </View>
+      </View>
+
+      <View style={styles.rowContainer}>
+        <Text style={styles.text}>Ghi chú</Text>
+        <TextInput
+          style={styles.input}
+          multiline={true}
+          numberOfLines={3}
+          placeholder="Ghi chú"
+        />
+      </View>
+
+      <Button title="Lưu" />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  rowContainer: {
+    flex: 1,
+    flexDirection: "row",
+
+    alignItems: "center",
+  },
+  rowContainerPicker: {
+    width: 375,
+    paddingLeft: 25,
+  },
+  text: {
+    flex: 1,
+    paddingLeft: 10,
+    paddingTop: 12,
+  },
+  textInput: {
+    flex: 1,
+    backgroundColor: "white",
+    borderColor: "black",
+  },
   input: {
-    height: 40,
+    height: 30,
+    width: 350,
     margin: 12,
     borderWidth: 1,
     padding: 10,
